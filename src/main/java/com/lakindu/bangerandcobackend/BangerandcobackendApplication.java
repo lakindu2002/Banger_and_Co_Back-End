301141f717c4252d4840851795ff148d15638a92
lakindu2002 <lakindu2002@live.com>
2021-03-24 14:34:00 +0530

    Initial Commit

---
+++ src/main/java/com/lakindu/bangerandcobackend/BangerandcobackendApplication.java
@@ -0,0 +1,13 @@
+package com.lakindu.bangerandcobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BangerandcobackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BangerandcobackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lakindu/bangerandcobackend/BangerandcobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lakindu.bangerandcobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BangerandcobackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
