3a0077ba1d27b642d1f2ac360925e8c9e648a14a
lakindu2002 <lakindu2002@live.com>
2021-03-30 17:00:31 +0530

    Added Bean Validation Exception to the Client

--- src/main/java/com/lakindu/bangerandcobackend/controller/GuestController.java
+++ src/main/java/com/lakindu/bangerandcobackend/controller/GuestController.java
@@ -3,20 +3,25 @@ package com.lakindu.bangerandcobackend.controller;
 import com.lakindu.bangerandcobackend.dto.InquiryDTO;
 import com.lakindu.bangerandcobackend.util.BangerAndCoExceptionHandler;
 import com.lakindu.bangerandcobackend.util.BangerAndCoResponse;
+import com.lakindu.bangerandcobackend.util.MultipleErrorSupporter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-
+import java.util.ArrayList;
 
 @RestController
-@RequestMapping(path = "/api/guest")
+@RequestMapping(path = "/api/guest") //base path of the api
 public class GuestController {
 
     @PostMapping(path = "/createInquiry")
     public ResponseEntity<BangerAndCoResponse> createInquiry(@Valid @RequestBody InquiryDTO theInquiry) {
+        //inquiry object is valid
+
         BangerAndCoResponse response = new BangerAndCoResponse("successfully recorded", HttpStatus.OK.value());
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
@@ -24,22 +29,33 @@ public class GuestController {
 
     @ExceptionHandler
     public ResponseEntity<BangerAndCoExceptionHandler> handlerDefaultException(Exception ex) {
-        System.out.println(ex);
+        //exception handler to handle all default exceptions thrown at runtime by JVM
         BangerAndCoExceptionHandler exceptionHandler = new BangerAndCoExceptionHandler(
                 "An error occurred on our end",
                 ex.getLocalizedMessage(),
-                HttpStatus.INTERNAL_SERVER_ERROR.value());
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                null);
 
         return new ResponseEntity<>(exceptionHandler, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler
     public ResponseEntity<BangerAndCoExceptionHandler> methodArgumentNotValid(MethodArgumentNotValidException ex) {
+        //custom exception handler for JPA Bean Validation Errors.
+        ArrayList<MultipleErrorSupporter> errorList = new ArrayList<>();
+        for (ObjectError err : ex.getBindingResult().getAllErrors()) {
+            //iterate the error map for the entity and retrieve field name and the error thrown.
+            errorList.add(new MultipleErrorSupporter(((FieldError) err).getField(), err.getDefaultMessage()));
+        }
+
+        //create an exception handler object
         BangerAndCoExceptionHandler exceptionHandler = new BangerAndCoExceptionHandler(
                 "Please Provide Valid Inputs for The Request Body",
-                ex.getMessage(),
-                HttpStatus.BAD_REQUEST.value()
+                ex.getLocalizedMessage(),
+                HttpStatus.BAD_REQUEST.value(),
+                errorList
         );
+        //return the response entity of type Bad Request back to the resource sending client
         return new ResponseEntity<>(exceptionHandler, HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/lakindu/bangerandcobackend/util/BangerAndCoExceptionHandler.java
+++ src/main/java/com/lakindu/bangerandcobackend/util/BangerAndCoExceptionHandler.java
@@ -1,14 +1,18 @@
 package com.lakindu.bangerandcobackend.util;
 
+import java.util.List;
+
 public class BangerAndCoExceptionHandler {
     private final String message;
     private final String exceptionMessage;
     private final int errorCode;
+    private final List<MultipleErrorSupporter> multipleErrors;
 
-    public BangerAndCoExceptionHandler(String message, String exceptionMessage, int errorCode) {
+    public BangerAndCoExceptionHandler(String message, String exceptionMessage, int errorCode, List<MultipleErrorSupporter> multipleErrors) {
         this.message = message;
         this.exceptionMessage = exceptionMessage;
         this.errorCode = errorCode;
+        this.multipleErrors = multipleErrors;
     }
 
     public String getMessage() {
@@ -21,5 +25,9 @@ public class BangerAndCoExceptionHandler {
 
     public int getErrorCode() {
         return errorCode;
+    }
+
+    public List<MultipleErrorSupporter> getMultipleErrors() {
+        return multipleErrors;
     }
 }
