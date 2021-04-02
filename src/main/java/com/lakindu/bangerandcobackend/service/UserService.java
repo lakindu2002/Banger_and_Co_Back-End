429ab390198b7fd3c9da0a01d1e1ba2ca749e48c
lakindu2002 <lakindu2002@live.com>
2021-04-02 14:00:14 +0530

    Updated Comments for Readability

--- src/main/java/com/lakindu/bangerandcobackend/service/UserService.java
+++ src/main/java/com/lakindu/bangerandcobackend/service/UserService.java
@@ -36,8 +36,10 @@ public class UserService {
         final User existingUser = theUserRepository.findUserByEmailAddress(theNewUser.getEmailAddress().toLowerCase());
 
         if (existingUser == null) {
+            //if user doesn't exist
             theNewUser.setEmailAddress(theNewUser.getEmailAddress().toLowerCase()); //save the lower case email in DB
 
+            //validate the Entity class for errors.
             DataBinder theDataBinder = new DataBinder(theNewUser);
             theDataBinder.addValidators((org.springframework.validation.Validator) validator);
             theDataBinder.validate();
@@ -45,15 +47,19 @@ public class UserService {
             final BindingResult theBindingResult = theDataBinder.getBindingResult();
 
             if (theBindingResult.hasErrors()) {
+                //if the entity class does not meet the expected validations
                 throw new ValidationException("Valid inputs were not provided for the fields during Sign Up.");
             } else {
+                //if successfully validated
                 theNewUser.setProfilePicture(profilePicture.getBytes());
                 ImageHandler theCompressor = new CompressImage(); //creating template method pattern
                 theCompressor.processUnhandledImage(theNewUser); //calling the template method
 
+                //retrieve role information for the customer
                 Role theRole = theRoleRepository.findRoleByRoleName("customer");
 
                 if (theRole != null) {
+                    //if the role has been retrieved successfully
                     theNewUser.setUserRole(theRole);
                     theNewUser.setBlackListed(false);
                     final User registeredUser = theUserRepository.save(theNewUser);
