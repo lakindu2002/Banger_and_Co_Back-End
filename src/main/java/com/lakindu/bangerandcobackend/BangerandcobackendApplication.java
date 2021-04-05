4e15d009f1ccd2f02a0270dd7e8a48a2271d10f5
lakindu2002 <lakindu2002@live.com>
2021-04-05 14:54:57 +0530

    Updated Email Content

--- src/main/java/com/lakindu/bangerandcobackend/BangerandcobackendApplication.java
+++ src/main/java/com/lakindu/bangerandcobackend/BangerandcobackendApplication.java
@@ -2,14 +2,7 @@ package com.lakindu.bangerandcobackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
-import java.util.Arrays;
-import java.util.Collections;
 
 @SpringBootApplication
 public class BangerandcobackendApplication {
