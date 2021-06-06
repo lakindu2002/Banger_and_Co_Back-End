1e917ae6f9077498399bc9b5967516f30be9a65d
lakindu2002 <lakindu2002@live.com>
2021-06-06 15:16:22 +0530

    retrieved parameters for vehicle creation

--- src/main/java/com/lakindu/bangerandcobackend/controller/VehicleController.java
+++ src/main/java/com/lakindu/bangerandcobackend/controller/VehicleController.java
@@ -8,9 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController //enables controller and a response body
 @RequestMapping(path = "/api/vehicle")
@@ -26,7 +25,12 @@ public class VehicleController {
     }
 
     @PostMapping(path = "/create", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<BangerAndCoResponse> createVehicle() {
+    public ResponseEntity<BangerAndCoResponse> createVehicle(
+            @RequestParam(name = "vehicleInformation", required = true) String jsonInput,
+            @RequestParam(name = "vehicleImage", required = true) MultipartFile vehicleImage
+    ) {
+        System.out.println(jsonInput);
+        System.out.println(vehicleImage);
         return new ResponseEntity<>(
                 new BangerAndCoResponse("Vehicle created successfully", HttpStatus.OK.value()),
                 HttpStatus.OK
