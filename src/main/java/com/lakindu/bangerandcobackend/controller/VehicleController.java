31a07de4c7d8001d8b435ec37e12b21c343f8609
lakindu2002 <lakindu2002@live.com>
2021-06-06 13:33:08 +0530

    added new endpoint

--- src/main/java/com/lakindu/bangerandcobackend/controller/VehicleController.java
+++ src/main/java/com/lakindu/bangerandcobackend/controller/VehicleController.java
@@ -1,5 +1,35 @@
 package com.lakindu.bangerandcobackend.controller;
 
+import com.lakindu.bangerandcobackend.serviceinterface.VehicleService;
+import com.lakindu.bangerandcobackend.util.exceptionhandling.BangerAndCoResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController //enables controller and a response body
+@RequestMapping(path = "/api/vehicle")
+@PreAuthorize("isAuthenticated()")
 public class VehicleController {
+    private final VehicleService vehicleService;
+
+    @Autowired
+    public VehicleController(
+            @Qualifier("vehicleServiceImpl") VehicleService vehicleService
+    ) {
+        this.vehicleService = vehicleService;
+    }
+
+    @PostMapping(path = "/create", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<BangerAndCoResponse> createVehicle() {
+        return new ResponseEntity<>(
+                new BangerAndCoResponse("Vehicle created successfully", HttpStatus.OK.value()),
+                HttpStatus.OK
+        );
+    }
 }
