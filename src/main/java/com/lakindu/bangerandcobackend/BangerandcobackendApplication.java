17a5d5efa24c8b9d3faba8fc14b05d37b906e0ca
Lakindu Hewawasam <lakindu2002@live.com>
2021-07-19 14:44:38 +0530

    added multi threading support for emails

--- src/main/java/com/lakindu/bangerandcobackend/BangerandcobackendApplication.java
+++ src/main/java/com/lakindu/bangerandcobackend/BangerandcobackendApplication.java
@@ -2,12 +2,26 @@ package com.lakindu.bangerandcobackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 
 @SpringBootApplication
+@EnableAsync //enable async operations
 public class BangerandcobackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BangerandcobackendApplication.class, args);
+    }
+
+    @Bean
+    public Executor executor() {
+        //configure a thread pool executor
+        ThreadPoolTaskExecutor theExecutor = new ThreadPoolTaskExecutor();
+        theExecutor.initialize(); //enable methods to utilize the @Async operation so that background operations can be carried out.
+        return theExecutor;
     }
 }
