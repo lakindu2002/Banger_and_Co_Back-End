82d88c9b9c26c75112b50ce2836ca9d25020f2ea
Lakindu Hewawasam <lakindu2002@live.com>
2021-08-22 09:59:00 +0530

    added dmv server integration

--- src/main/java/com/lakindu/bangerandcobackend/util/scheduledjobs/ScheduledJobsManagement.java
+++ src/main/java/com/lakindu/bangerandcobackend/util/scheduledjobs/ScheduledJobsManagement.java
@@ -1,19 +1,24 @@
 package com.lakindu.bangerandcobackend.util.scheduledjobs;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.http.client.ClientHttpRequest;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.support.CronExpression;
 import org.springframework.stereotype.Component;
 import org.springframework.util.MultiValueMap;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.PostConstruct;
+import java.io.*;
 import java.net.URI;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.Base64;
+import java.util.Date;
+import java.util.logging.Logger;
 
 @Component
 public class ScheduledJobsManagement {
@@ -26,8 +31,13 @@ public class ScheduledJobsManagement {
     @Value("${custom.dmv.endpoint}")
     private String apiEndPoint;
 
+    @Value("${custom.dmv.filename}")
+    private String saveFileName;
+
     private String basicAuthHeader;
     private HttpHeaders httpHeaders;
+    private Logger LOGGER = Logger.getLogger(ScheduledJobsManagement.class.getName());
+    private String folderForCsv = System.getProperty("user.dir") + "/csv";
 
     @PostConstruct
     public void constructAuthHeader() {
@@ -37,11 +47,65 @@ public class ScheduledJobsManagement {
         basicAuthHeader = Base64.getEncoder().encodeToString(credentialsToBeEncoded.getBytes(StandardCharsets.UTF_8));
         httpHeaders = new HttpHeaders();
         httpHeaders.add("Authorization", String.format("Basic %s", basicAuthHeader));
+
+        File theFolder = new File(folderForCsv);
+        if (!theFolder.exists()) {
+            theFolder.mkdir(); //create the folder if it doesn't exist.
+        }
     }
 
-    //cron(Minutes Hours Day-of-month Month Day-of-week Year)
     //scheduled to run everyday at 12:01AM
-    @Scheduled(cron = "* * * * * *")
+    //seconds - minute - hour - day of month - month - day of week
+    @Scheduled(cron = "00 01 00 * * *")
     public void fetchDmvCsv() {
+        try {
+            LOGGER.info("Triggering DMV Get CSV Scheduled Job at: " + new Date().toString());
+            RestTemplate dmvCall = new RestTemplate();
+            //pass the basic auth header
+            HttpEntity<?> headerEntity = new HttpEntity<>(httpHeaders);
+
+            //dmv get call
+            ResponseEntity<byte[]> getCsvResponse = dmvCall.exchange(
+                    URI.create(apiEndPoint),
+                    HttpMethod.GET,
+                    headerEntity,
+                    byte[].class
+            );
+
+            if (getCsvResponse.getStatusCode() == HttpStatus.OK) {
+
+                if (getCsvResponse.hasBody()) {
+                    //csv file present
+                    File theCsv = new File(folderForCsv + File.separator + saveFileName);
+                    LOGGER.info("Attempting to Save In: " + theCsv.getPath());
+                    if (theCsv.exists()) {
+                        //if csv exists, delete the file
+                        theCsv.delete();
+                        LOGGER.info("Scheduled Job Deleted Existing CSV at: " + new Date().toString());
+                    } else {
+                        theCsv.createNewFile(); //create new file
+                    }
+
+                    if (getCsvResponse.getBody() != null) {
+                        //if the csv file is present, flush the binary data to the licenseList.csv located in CSV directory
+                        FileOutputStream theOutputStream = new FileOutputStream(theCsv);
+                        theOutputStream.write(getCsvResponse.getBody()); //write the response body, which is the csv returned from server
+                        theOutputStream.flush(); //flush contents of stream to the file
+                        theOutputStream.close(); //close the stream
+                        LOGGER.info("Scheduled Job Successfully Saved CSV at: " + new Date().toString());
+                    } else {
+                        LOGGER.warning("CSV Body is null");
+                    }
+
+                } else {
+                    //if ran into an error (return code anything other than 200)
+                    LOGGER.warning("DMV Call Returned No CSV at: " + new Date().toString());
+                }
+            } else {
+                LOGGER.warning("Call Returned With Response: " + getCsvResponse.getStatusCode().toString());
+            }
+        } catch (Exception ex) {
+            LOGGER.warning("We ran into an error while fetching the CSV: " + ex.getMessage());
+        }
     }
 }
