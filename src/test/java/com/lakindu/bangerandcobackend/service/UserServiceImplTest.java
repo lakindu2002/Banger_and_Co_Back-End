176bc5e9ee64ceb7ebe5782c073a455cf7d65abd
Lakindu Hewawasam <lakindu2002@live.com>
2021-09-10 10:13:24 +0530

    added userserviceimpl and vehicleserviceimpl test

---
+++ src/test/java/com/lakindu/bangerandcobackend/service/UserServiceImplTest.java
@@ -0,0 +1,80 @@
+package com.lakindu.bangerandcobackend.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceImplTest {
+
+    @Test
+    void _getUserWithImageDecompression() {
+    }
+
+    @Test
+    void _getUserWithoutDecompression() {
+    }
+
+    @Test
+    void _getUserRole() {
+    }
+
+    @Test
+    void getUserInformation() {
+    }
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void updateUserInformation() {
+    }
+
+    @Test
+    void getAllCustomers() {
+    }
+
+    @Test
+    void whitelistCustomer() {
+    }
+
+    @Test
+    void updateCustomerLicenseImage() {
+    }
+
+    @Test
+    void getCustomerLicenseImage() {
+    }
+
+    @Test
+    void getCustomerOtherImage() {
+    }
+
+    @Test
+    void updateCustomerOtherImage() {
+    }
+
+    @Test
+    void getAllAdministrators() {
+    }
+
+    @Test
+    void createAdmin() {
+    }
+
+    @Test
+    void removeAdministrator() {
+    }
+
+    @Test
+    void blackListCustomer() {
+    }
+
+    @Test
+    void _getAllAdminEmails() {
+    }
+
+    @Test
+    void _blackListCustomer() {
+    }
+}
---
+++ src/test/java/com/lakindu/bangerandcobackend/service/VehicleServiceImplTest.java
@@ -0,0 +1,54 @@
+package com.lakindu.bangerandcobackend.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VehicleServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void createVehicle() {
+    }
+
+    @Test
+    void getAllVehicles() {
+    }
+
+    @Test
+    void getAllVehiclesThatCanBeRentedForGivenPeriod() {
+    }
+
+    @Test
+    void isVehicleAvailableOnGivenDates() {
+    }
+
+    @Test
+    void _getVehicleInformation() {
+    }
+
+    @Test
+    void removeVehicleById() {
+    }
+
+    @Test
+    void checkIfVehicleHasPendingOrOnGoingRentals() {
+    }
+
+    @Test
+    void updateVehicle() {
+    }
+
+    @Test
+    void getVehicleById() {
+    }
+}
